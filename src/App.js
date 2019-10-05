22a81732d360752931d70691a7939cbfaf6f260d
swaroop <swaroopanand325@gmail.com>
2019-10-05 19:47:03 +0530

    Added localstorage object for emi history save

--- src/App.js
+++ src/App.js
@@ -5,34 +5,45 @@ import 'react-rangeslider/lib/index.css'
 import styles from './style';
 import './App.css';
 import 'tachyons';
+import uuid from 'uuid';
 
 const particlesOptions = {
   "particles": {
-      "number": {
-          "value": 70
-      },
-      "size": {
-          "value": 3
-      }
+    "number": {
+      "value": 70
+    },
+    "size": {
+      "value": 3
+    }
   },
   "interactivity": {
-      "events": {
-          "onhover": {
-              "enable": true,
-              "mode": "repulse"
-          }
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
       }
+    }
   }
 }
 
 class App extends Component {
-  constructor (props, context) {
+  constructor(props, context) {
     super(props, context)
     this.state = {
       value: 500,
-      value2: 6
+      value2: 6,
+      loanDetails: {},
+      history: [],
     }
   }
+  componentWillMount(){
+    if(localStorage.getItem('loanHistory')){
+      this.setState({
+        history: JSON.parse(localStorage.getItem('loanHistory'))
+      })
+    }
+  }
+  
   amountChange = value => {
     this.setState({
       value: value
@@ -50,29 +61,46 @@ class App extends Component {
     this.calculate();
   };
 
-  calculate = () =>{
-    let amt=this.state.value;
-    let mon=this.state.value2;
-    console.log(amt,mon);
-    fetch("https://ftl-frontend-test.herokuapp.com/interest?amount="+amt+"&numMonths="+mon)
-    .then(response=> response.json())
-    .then((result)=>{
-      console.log(result.interestRate)
-      console.log(result.monthlyPayment.amount)
-      document.getElementById('rate').value=result.interestRate
-      document.getElementById('pay').value=result.monthlyPayment.amount
+  addHistory = () => {
+    this.setState(state => {
+      let lD = state.loanDetails;
+      lD.id = uuid.v4();
+      const arr = state.history.concat(lD);
+      return {
+        isLoading: false,
+        loanDetails: state.loanDetails,
+        history: arr
+      }
     })
+  };
+
+  calculate = () => {
+    var that = this;
+    let amt = this.state.value;
+    let mon = this.state.value2;
+    console.log(amt, mon);
+    fetch("https://ftl-frontend-test.herokuapp.com/interest?amount=" + amt + "&numMonths=" + mon)
+      .then(response => response.json())
+      .then((result) => {
+        console.log(result.interestRate)
+        console.log(result.monthlyPayment.amount)
+        document.getElementById('rate').value = result.interestRate
+        document.getElementById('pay').value = result.monthlyPayment.amount
+        this.setState({ loanDetails: result })
+        this.addHistory()
+        localStorage.setItem('loanHistory', JSON.stringify(that.state.history));
+      })
   }
 
-  render () {    
-    const { value,value2 } = this.state
+  render() {
+    const { value, value2 } = this.state
     return (
       <div style={styles.root}>
         <h1 className='f1'>EMI CALCULATOR</h1>
         <Particles className='particles'
           params={particlesOptions}
         />
-        <div className='value b f4' style={styles.sliderborder}>Amount in $  {value}</div>        
+        <div className='value b f4' style={styles.sliderborder}>Amount in $  {value}</div>
         <div style={styles.sliderWrapper}>
           <div className='slider'>
             <Slider
@@ -83,7 +111,7 @@ class App extends Component {
               onChangeComplete={this.handleChangeComplete}
             />
           </div>
-        </div>  
+        </div>
 
         <div className='value b f4' style={styles.sliderborder}>Months:  {value2}</div>
         <div style={styles.sliderWrapper}>
@@ -96,33 +124,33 @@ class App extends Component {
               onChangeComplete={this.handleChangeComplete}
             />
           </div>
-        </div> 
+        </div>
 
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0 b">EMI DETAILS</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f4 b" htmlFor="email-address">Interest Rate(%)</label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 black b"
-                  id="rate"
-                  disabled
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f4 b" htmlFor="password">Monthly payment in USD</label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 black b"
-                  id="pay"
-                  disabled 
-                />
-              </div>
-            </fieldset>
-          </div>
-        </main>
-      </article>        
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+          <main className="pa4 black-80">
+            <div className="measure">
+              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className="f1 fw6 ph0 mh0 b">EMI DETAILS</legend>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f4 b" htmlFor="email-address">Interest Rate(%)</label>
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 black b"
+                    id="rate"
+                    disabled
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f4 b" htmlFor="password">Monthly payment in USD</label>
+                  <input
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 black b"
+                    id="pay"
+                    disabled
+                  />
+                </div>
+              </fieldset>
+            </div>
+          </main>
+        </article>
 
       </div>
     )
